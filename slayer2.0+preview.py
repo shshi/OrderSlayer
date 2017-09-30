071873b9ebdd494d28b9414f03b1fda66a2b4844
Shaohua <cell.fantasy@qq.com>
2017-09-30 21:37:30 +0700

    Update slayer2.0+preview.py

--- slayer2.0+preview.py
+++ slayer2.0+preview.py
@@ -53,12 +53,12 @@ def hunt():
         if isElementExist("mCSB_1_container"): #判断是否存在“预览”，亦即判断是否有单           
             if b.find_element_by_xpath('//*[@id="mCSB_1_container"]/div/a').is_displayed(): #判断“预览”是否显示，亦即判断是否有新单          
                 print 'new order found'
+                b.find_element_by_xpath('//*[@id="mCSB_1_container"]/div/a').click() #点击“预览”
                 if YN:
                     txt_word = b.find_element_by_xpath("//*[@class='words col-xs-12 col-md-2 nonePadding']").text[0:-3] #获取订单字数
                     num_word = float (txt_word) #转换订单字数为数值类型
                     if num_word <= limit:
                         print "%s words order"%txt_word
-                        b.find_element_by_xpath('//*[@id="mCSB_1_container"]/div/a').click() #点击“预览”
 
                         #保存页面源码
                         page = b.page_source.encode('gbk', 'ignore')
