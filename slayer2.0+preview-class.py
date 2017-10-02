8cec2f0e1ea4fadf2d71790a98883bbea8c22b2e
Shaohua <cell.fantasy@qq.com>
2017-10-02 14:33:48 +0700

    Add files via upload

---
+++ slayer2.0+preview-class.py
@@ -0,0 +1,112 @@
+#-*- coding: utf-8 -*-
+#===========================================================
+# Author：Sha0hua
+# E-mail:shi.sh@foxmail.com
+# Modified Date: 2017-09-27
+# Version: 2.0
+# Version Description: added choice of word limit 
+#===========================================================
+from selenium import webdriver
+import time
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class OS():
+    url = ''
+    def __init__(self, url):
+        self.url = url
+
+    def pre(self):
+        print "some preparation work..."
+        sys.setrecursionlimit(100000) #设置最大递归次数（若不设置，默认值为998，递归998次后将出现"maximum recursion depth exceeded"的报错）
+        global b
+        b=webdriver.PhantomJS('phantomjs') #无浏览器模式
+        #b=webdriver.Firefox() #浏览器可视模式
+        b.set_window_size(1600, 900)
+        url = self.url
+        b.get(url) #WE登录页        
+      
+    def login(self):
+        print "logging in..."
+        b.find_element_by_id("loginPhone").send_keys("18209347100") #输入手机号
+        b.find_element_by_id("loginPassword").send_keys("ssh19198918") #输入密码
+        b.find_element_by_xpath("//*[@onclick='login()']").click() #触发登录
+        b.get("http://talent.woordee.com/front/task/taskCenter") #进入"订单中心"页面
+        print "successfully logged in"
+
+    def YN(self):
+        yn = raw_input('need to set a word limit?[Y/N] ')
+        if yn != "Y" and yn != "N":
+            print "wrong input, please input again(just type 'Y' or 'N') "
+            self.YN()
+        elif yn == "Y":
+            global limit
+            limit = input('please input word limit: ') #输入接单字数限制       
+            return True
+        else:
+            return False
+
+    def isElementExist(self, element):
+        try:
+            b.find_element_by_id(self, element)
+            return True
+        except:
+            return False
+
+    def slay(self):
+        try:
+            b.find_element_by_link_text("领取订单").click()
+            print 'slayed'
+            print '\a' #播放提示音
+            time.sleep(7)
+        except:
+            print "preview time"
+            self.slay()
+
+    def hunt(self):
+        try:
+            if self.isElementExist("mCSB_1_container"): #判断是否存在“预览”，亦即判断是否有单           
+                if b.find_element_by_xpath('//*[@id="mCSB_1_container"]/div/a').is_displayed(): #判断“预览”是否显示，亦即判断是否有新单          
+                    print 'new order found'
+                    b.find_element_by_xpath('//*[@id="mCSB_1_container"]/div/a').click() #点击“预览”
+                    if YN:
+                        txt_word = b.find_element_by_xpath("//*[@class='words col-xs-12 col-md-2 nonePadding']").text[0:-3] #获取订单字数
+                        num_word = float (txt_word) #转换订单字数为数值类型
+                        if num_word <= limit:
+                            print "%s words order"%txt_word
+
+                            #保存页面源码
+                            page = b.page_source.encode('gbk', 'ignore')
+                            log = open('previewPage.log', 'a')
+                            log.write(page)
+                            log.close()
+
+                            self.slay()
+                        else:
+                            print "over %d words, let it go"%limit
+                            b.refresh()
+                            self.hunt()
+                    else:
+                        self.slay()
+                else:
+                    b.refresh()
+                    self.hunt()
+            else:
+                b.refresh() #刷新页面
+                self.hunt()
+        except Exception as e:
+            print e
+            b.refresh()
+            time.sleep(3)
+            print 'continue hunting...'
+            self.hunt()
+
+if __name__ == '__main__':
+    OS = OS("http://talent.woordee.com/front/truser")
+    OS.pre()
+    OS.login()
+    YN = OS.YN()
+    print "hunting..."
+    OS.hunt()
