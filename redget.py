2c9721b9c564b5c336c8da592a3b0e9171ef6ae6
Shaohua <cell.fantasy@qq.com>
2018-06-05 14:56:53 +0700

    Update redget.py

--- redget.py
+++ redget.py
@@ -16,24 +16,23 @@ sys.setdefaultencoding('utf-8')
 
 def sign(b): 
     b.find_element_by_id("loginPhone").send_keys("18209347100") #输入手机号
-    b.find_element_by_id("loginPassword").send_keys("ssh19198918") #输入密码
+    b.find_element_by_id("password").send_keys("ssh19198918") #输入密码
     b.find_element_by_xpath("//*[@onclick='login()']").click() #触发登录
+    #b.find_element_by_xpath("//*[@class='btn-sign']").click() #触发签到
+    #b.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/div/div/a").click()
     b.find_element_by_class_name("btn-sign").click()
+    #b.find_element_by_xpath('//*[@id="timeAtag"]').click()
     
-#b=webdriver.PhantomJS('phantomjs')
-#b.set_window_size(1600, 900)
-b=webdriver.Firefox()
+b=webdriver.PhantomJS('phantomjs')
+b.set_window_size(1600, 900)
+#b=webdriver.Firefox()
 b.get("http://talent.woordee.com/front/truser") #WE登录页
 sign(b)
-for i in range(1500):
+for i in range(120):
     print i
     b.find_element_by_xpath('//*[@id="timeAtag"]').click()
-    #b.refresh()
-    if i < 1499:
+    time.sleep(1)
+    if i < 119:
         print "looping"
     else:
         print "finished!"
-
-
-
-
