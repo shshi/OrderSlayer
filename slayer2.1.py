a2538ef463f9a7556e87e9cbb643b93544112004
Shaohua <cell.fantasy@qq.com>
2018-06-06 18:04:10 +0700

    Update slayer2.1.py

--- slayer2.1.py
+++ slayer2.1.py
@@ -60,6 +60,7 @@ def isElementExist(element):
         return False
 
 def slay():
+    b.set_page_load_timeout(7)
     try:
         b.find_element_by_link_text("领取订单").click()
         print 'slayed'
