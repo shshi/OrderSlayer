494f8537f52dc2abf329904999fcbfb3f4ff47de
Shaohua <cell.fantasy@qq.com>
2018-06-09 12:53:37 +0700

    Update sign2.0.py

--- sign2.0.py
+++ sign2.0.py
@@ -41,50 +41,5 @@ def sign():
             print (txt_bf)
         except Exception as e:
             print (e)
-'''
-        #签到操作
-        s.post('http://talent.woordee.com/front/truser/sign', sgn_data) #触发签到  
 
-        #提取签到结果并打印
-        page = s.get('http://talent.woordee.com/front/square').content #重新get地址并获取页面源码
-        soup = BeautifulSoup(page,"html.parser")
-        txt1 = soup.find_all('a', attrs={"id":"ySign"})[0].get_text().strip() #提取“已签到”文本
-        print (txt1)
-        #if len(txt1)==3:
-        string = “已签到”
-        if tx1==string.encode(‘utf-8’):
-            print ("Successfully signed")
-        else:
-            sendMail()
-
-        txt_aft = soup.find_all('em', attrs={"class":"signedCount"})[0].get_text()
-        num_aft = re.findall(r"\d+", txt_aft)
-        print (txt_aft)
-        if num_bf<num_aft:
-            print ("%ddays signed consecutively"%num_aft)
-        else:
-            pass
-            #sendMail()
-    except:
-        pass
-        #sendMail()
-
-def sendMail():        
-    print ("Failed in signing")
-    msg = MIMEMultipart()
-    body = MIMEText("ERROR happened from running sign.py, please check on Heroku immediately!")
-    msg.attach(body)
-    msg['Subject'] = 'ERROR happened from running sign.py on Heroku!'
-    msg['From'] = "sign.Heroku<cell.fantasy@qq.com>"
-    msg['To'] = "shi.sh@foxmail.com"
-    try:
-        s = smtplib.SMTP_SSL("smtp.qq.com", 465)
-        s.connect("smtp.qq.com")
-        s.login("cell.fantasy","Ssh31415926")
-        s.sendmail("sign.Heroku<cell.fantasy@qq.com>", "shi.sh@foxmail.com", msg.as_string())
-        s.close()
-        print ("Successfully sent to %s"%msg['to'])
-    except Exception as e:
-        print (e)
-'''    
 sign()
