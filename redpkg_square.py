9fafcc685f2d7b69975c11d92256f33511268f97
Shaohua <cell.fantasy@qq.com>
2018-06-15 01:29:14 +0700

    Update redpkg_square.py

--- redpkg_square.py
+++ redpkg_square.py
@@ -1,68 +1,48 @@
-# -*- coding:utf-8 -*-
+#-*- coding: utf-8 -*-
 #===========================================================
+# Name: redget
 # Author：Sha0hua
 # E-mail:shi.sh@foxmail.com
-# Created Date: 2017-09-07
-# Modified Date: 2017-09-19
-# Version: 2.0
-# Description: sign at Woordee website  
+# Modified Date: 2018-06-14
+# Version: 1.0
+# Description: get red packet from talent.woordee.com
 #===========================================================
-import requests
-from bs4 import BeautifulSoup
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.options import Options
 import time
+import logging
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+sys.setrecursionlimit(1000000000)
 
 def sign():
-    try:
-        #登录及签到post数据准备
-        s = requests.session()
-        log_data = {'loginPhone':'18209347100','loginPassword':'ssh19198918'} #登录post数据
-        sgn_data = {'translatorId':'WE16104633TR'} #签到post数据
-    
-        #登录操作
-        log = s.post('http://talent.woordee.com/front/truser/login', log_data) #post登录地址 
-        html = s.get('http://talent.woordee.com/front/square') #get登陆后的地址        
+    d.get("http://talent.woordee.com/front/truser.html")
+    print "logging in..."
+    #d.set_page_load_timeout(10)
+    d.find_element_by_id("loginPhone").send_keys("18209347100") #输入手机号
+    d.find_element_by_id("password").send_keys("ssh19198918") #输入密码
+    d.find_element_by_xpath("//*[@onclick='login()']").click() #触发登录
+    print "successfully logged in"
+       
+def getRed():
+    d.get("http://talent.woordee.com/front/square.html")
+    while d.find_element_by_xpath('/html/body/div[2]/div[1]/ul/li[1]/div[2]/p[1]').is_displayed():
+        print "not yet"
+    print "now!"
+    d.find_element_by_xpath('//*[@id="timeAtag"]').click()
+    d.find_element_by_xpath('/html/body/div[2]/div[1]/ul/li[2]/div[1]/a').click()   
+    print "done"
 
-        for i in range(60):
-            print (i)
-            s.post('http://talent.woordee.com/front/truser/sign', sgn_data) #触发签到
-            time.sleep(1)
-            if i < 59:
-                print ("looping")
-            else:
-                print ("finished!")
-
-        #提取签到结果并打印
-        page = s.get('http://talent.woordee.com/front/square').content #重新get地址并获取页面源码
-        soup = BeautifulSoup(page,"html.parser")
-        txt1 = soup.find_all('a', attrs={"id":"ySign"})[0].get_text().strip() #提取“已签到”文本
-        #txt2 = soup.find_all('em', attrs={"class":"signedCount"})[0].get_text() #提取“连续签到n天”文本
-        print (txt1)
-        if len(txt1)==3:
-            print ("Successfully signed")
-        else:
-            sendMail()
-    except:
-        sendMail()
-
-def sendMail():        
-    print ("Failed in signing")
-    msg = MIMEMultipart()
-    body = MIMEText("ERROR happened from running sign.py, please check on Heroku immediately!")
-    msg.attach(body)
-    msg['Subject'] = 'ERROR happened from running sign.py on Heroku!'
-    msg['From'] = "sign.Heroku<cell.fantasy@qq.com>"
-    msg['To'] = "shi.sh@foxmail.com"
-    try:
-        s = smtplib.SMTP_SSL("smtp.qq.com", 465)
-        s.connect("smtp.qq.com")
-        s.login("cell.fantasy","Ssh31415926")
-        s.sendmail("sign.Heroku<cell.fantasy@qq.com>", "shi.sh@foxmail.com", msg.as_string())
-        s.close()
-        print ("Successfully sent to %s"%msg['to'])
-    except Exception as e:
-        print (e)
-
-sign()
+if __name__ == "__main__":
+    firefoxProfile = FirefoxProfile()
+    #firefoxProfile.set_preference('permissions.default.stylesheet', 2) #禁加载CSS
+    firefoxProfile.set_preference('permissions.default.image', 2) #禁加载图片
+    firefoxProfile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false') #禁加载Flash
+    options = Options()
+    #options.add_argument('-headless') #无浏览器参数
+    d=webdriver.Firefox(firefoxProfile, firefox_options=options)
+    d.set_window_size(1600, 900)
+    sign()
+    getRed()
