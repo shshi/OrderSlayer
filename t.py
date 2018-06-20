502fc437a73ad29a3172eea6135c06051e1bc966
Shaohua <cell.fantasy@qq.com>
2018-06-20 16:48:21 +0700

    Update t.py

--- t.py
+++ t.py
@@ -61,7 +61,7 @@ def sign():
         page = s.get('http://talent.woordee.com/getSignData', headers=headers_square).content #重新get地址并获取页面源码
         print (page)
         print (type(page))
-        stringed=str(page, encoding = "utf-8")  
+        stringed=page.decode() 
         dic=eval(stringed)
         print (dic)
         print (dic["hasSigned"])
