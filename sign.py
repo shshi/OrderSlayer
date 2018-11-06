5750617a544d62e77b5d21dd0766544e89d84904
Shaohua <cell.fantasy@qq.com>
2018-11-06 13:26:15 +0700

    Update sign.py

--- sign.py
+++ sign.py
@@ -17,23 +17,23 @@ def sign():
         #登录及签到post数据准备
         s = requests.session()        
         headers_login = {"Host":"talent.woordee.com", "Connection":"keep-alive", "Content-Length":"125",
-                   "Accept":"*/*", "Origin":"http://talent.woordee.com", "X-Requested-With":"XMLHttpRequest",
+                   "Accept":"*/*", "Origin":"https://talent.woordee.com", "X-Requested-With":"XMLHttpRequest",
                    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36",
                    "Content-Type":"application/x-www-form-urlencoded",
-                   "Referer":"http://talent.woordee.com/front/truser.html",
-                   "Accept-Encoding":"gzip, deflate",
-                   "Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8",
-                   "Cookie": "SESSION=6fdb29a3-8943-42af-b7dc-61c3d5b14558"
-                  }
-        login_data = {'loginPhone':'18209347100','loginPassword':'11221135d35eacd2de7b136d15be0662','loginLowerCasePassword':'11221135d35eacd2de7b136d15be0662'} #登录post数据        
-        headers_sign = {"Host":"talent.woordee.com", "Connection":"keep-alive", "Content-Length":"25",
-                   "Accept":"*/*", "Origin":"http://talent.woordee.com", "X-Requested-With":"XMLHttpRequest",
-                   "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36",
-                   "Content-Type":"application/x-www-form-urlencoded",
-                   "Referer":"http://talent.woordee.com/front/square",
+                   "Referer":"https://talent.woordee.com/users/login",
                    "Accept-Encoding":"gzip, deflate",
                    "Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8",
                    "Cookie": "_uab_collina=154139283253526626410393; SESSION=6fdb29a3-8943-42af-b7dc-61c3d5b14558"
+                  }
+        login_data = {'loginPhone':'18209347100','loginPassword':'11221135d35eacd2de7b136d15be0662','loginLowerCasePassword':'11221135d35eacd2de7b136d15be0662'} #登录post数据        
+        headers_sign = {"Host":"talent.woordee.com", "Connection":"keep-alive", "Content-Length":"0",
+                   "Accept":"*/*", "Origin":"https://talent.woordee.com", "X-Requested-With":"XMLHttpRequest",
+                   "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36",
+                   "Content-Type":"application/x-www-form-urlencoded",
+                   "Referer":"https://talent.woordee.com/square/center",
+                   "Accept-Encoding":"gzip, deflate",
+                   "Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8",
+                   "Cookie": "SESSION=6fdb29a3-8943-42af-b7dc-61c3d5b14558"
                   }
         sign_data = {'translatorId':'WE16104633TR'} #签到post数据        
         headers_square = {"Host":"talent.woordee.com", "Connection":"keep-alive", "Upgrade-Insecure-Requests":"1",
@@ -47,7 +47,7 @@ def sign():
                   }
     
         #登录操作
-        log = s.post('https://talent.woordee.com/users/login', data=login_data, headers=headers_login) #post登录地址
+        log = s.post('https://talent.woordee.com/users/doLogin', data=login_data, headers=headers_login) #post登录地址
         
         #签到前数据获取
         print ("logged in")
