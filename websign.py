1885bcf8286712f7e695eeb0534f5e03e7566491
Shaohua <cell.fantasy@qq.com>
2019-03-22 22:44:55 +0700

    Update websign.py

--- websign.py
+++ websign.py
@@ -1,75 +1,113 @@
 #-*- coding: utf-8 -*-
 import flask
-import urllib.request as u
-import base64
+import requests
+import time
+from random import randint
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = flask.Flask(__name__)
 @app.route("/")
 
-def getList():
-    #f = open("proxyList.log",'w',encoding='utf-8')
-    url="https://raw.githubusercontent.com/AmazingDM/sub/master/ssrshare.com"
-    page = u.urlopen(url)
-    html = page.read().decode('UTF-8')
-
-    SSR_list=base64.b64decode(html).decode('utf-8')
-    SSR_list=SSR_list.strip()   
-    lst=SSR_list.splitlines()
-    list_sum=""
-    for i in lst:
-        try:
-            base64_encode_str = i[6:]
-            decode_str = base64_decode(base64_encode_str)
-            parts = decode_str.split(':')
-            if len(parts) != 6:
-                print('不能解析SSR链接: %s' % base64_encode_str)
-
-            server = parts[0]
-            port = parts[1]
-            protocol = parts[2]
-            method = parts[3]
-            obfs = parts[4]
-            password_and_params = parts[5]
-
-            password_and_params = password_and_params.split("/?")
-
-            password_encode_str = password_and_params[0]
-            password = base64_decode(password_encode_str)
-            params = password_and_params[1]
-
-            param_parts = params.split('&')
-
-            param_dic = {}
-            for part in param_parts:
-               key_and_value = part.split('=')
-               param_dic[key_and_value[0]] = key_and_value[1]
-
-            obfsparam = base64_decode(param_dic['obfsparam'])
-            protoparam = base64_decode(param_dic['obfsparam'])
-            remarks = base64_decode(param_dic['remarks'])
-            group = base64_decode(param_dic['group'])
-
-            lst_item='server: %s, port: %s, 协议: %s, 加密方法: %s, 密码: %s, 混淆: %s, 混淆参数: %s, 协议参数: %s, 备注: %s, 分组: %s\r\n'% (server, port, protocol, method, password, obfs, obfsparam, protoparam, remarks, group)
-            list_sum+=lst_item
+def sign():
+    sec=randint(1800,7000)
+    hour='%.2f'%(sec/3600)
+    timecount='waiting time: %d sec, %s hour'%(sec, hour)
+    print (timecount)
+    time.sleep(sec)
+    try:
+        s = requests.session()
+        headers_resp = {"Host":"talent.woordee.com", "Connection":"keep-alive", "Cache-Control":"max-age=0",
+                   "Upgrade-Insecure-Requests":"1", "Origin":"https://talent.woordee.com", "X-Requested-With":"XMLHttpRequest",
+                   "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36",
+                   "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                   "Accept-Encoding":"gzip, deflate, br",
+                   "Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8"
+                  }
+        headers_login = {"Host":"talent.woordee.com", "Connection":"keep-alive", "Content-Length":"125",
+                   "Accept":"*/*", "Origin":"https://talent.woordee.com", "X-Requested-With":"XMLHttpRequest",
+                   "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36",
+                   "Content-Type":"application/x-www-form-urlencoded",
+                   "Referer":"https://talent.woordee.com/users/login",
+                   "Accept-Encoding":"gzip, deflate, br",
+                   "Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8"
+                  }
+        login_data = {'loginPhone':'18209347100','loginPassword':'11221135d35eacd2de7b136d15be0662','loginLowerCasePassword':'11221135d35eacd2de7b136d15be0662'} #ç»å½postæ°æ®        
+        headers_sign = {"Host":"talent.woordee.com", "Connection":"keep-alive", "Content-Length":"0",
+                   "Accept":"*/*", "Origin":"https://talent.woordee.com", "X-Requested-With":"XMLHttpRequest",
+                   "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36",
+                   "Content-Type":"application/x-www-form-urlencoded",
+                   "Referer":"https://talent.woordee.com/square/center",
+                   "Accept-Encoding":"gzip, deflate, br",
+                   "Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8"
+                  }
+        sign_data = {'translatorId':'WE16104633TR'}       
+    
+        #get cookie
+        kk = s.get('https://talent.woordee.com/users/login', headers=headers_resp).cookies.get_dict()
+        #print (kk)
+        kk_re = '\"Cookie\": ' + '\"SESSION=' + kk['SESSION'] + '\"'
+        print (kk_re)
+        
+        #login
+        log = s.post('https://talent.woordee.com/users/doLogin', data=login_data, headers=headers_login)
+        print ("logged in")
+        
+        #get the sign count before sign
+        s.post('https://talent.woordee.com/checkLogin', headers=headers_sign)
+        countBf=s.post('https://talent.woordee.com/square/operate/signdetail', headers=headers_sign).json()
+        #print (countBf)
+        signBf=countBf['signInfo']['sign']
+        print (signBf)
+        
+        #sign
+        #s.post('https://talent.woordee.com/checkLogin', headers=headers_sign)
+        #s.post('https://talent.woordee.com/square/operate/signdetail', headers=headers_sign)
+        #s.post('https://talent.woordee.com/checkLogin', headers=headers_sign)
+        s.post('https://talent.woordee.com/square/operate/sign', headers=headers_sign) #signing  
+        print ("signing executed")
+        
+        #get the sign count after sign
+        s.post('https://talent.woordee.com/checkLogin', headers=headers_sign)
+        countAft=s.post('https://talent.woordee.com/square/operate/signdetail', headers=headers_sign).json()
+        #print (countAft)
+        result=countAft['signInfo']['sign']
+        print (result)
+        
+        #judge the result
+        if result:
+            return "congrats\n"+result+"\n"timecount
+        else:
+            global E
+            E='result==False, signing failed'
+            sendMail()
             
-        except Exception as e:
-            print (e)
-            continue
-    print (list_sum)
-    return list_sum
-def fill_padding(base64_encode_str):
+    except Exception as e:
+        E=str(e)
+        return e
+        sendMail()
 
-   need_padding = len(base64_encode_str) % 4 != 0
+def sendMail():        
+    print ("Failed in signing")
+    msg = MIMEMultipart()
+    body = MIMEText("ERROR happened from running sign.py, please check on Heroku immediately!\n\n" + "Detail:\n" + E)
+    msg.attach(body)
+    msg['Subject'] = 'ERROR happened from running sign.py on Heroku!'
+    msg['From'] = "sign.Heroku<cell.fantasy@qq.com>"
+    msg['To'] = "shi.sh@foxmail.com"
+    try:
+        s = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        s.connect("smtp.qq.com")
+        s.login("cell.fantasy","Ssh31415926")
+        s.sendmail("sign.Heroku<cell.fantasy@qq.com>", "shi.sh@foxmail.com", msg.as_string())
+        s.close()
+        print ("Successfully sent to %s"%msg['to'])
+    except Exception as e:
+        print (e)
+    
+sign()
 
-   if need_padding:
-       missing_padding = 4 - need_padding
-       base64_encode_str += '=' * missing_padding
-   return base64_encode_str
-
-
-def base64_decode(base64_encode_str):
-   base64_encode_str = fill_padding(base64_encode_str)
-   return base64.urlsafe_b64decode(base64_encode_str).decode('utf-8')
 
 
 if __name__ == '__main__':
