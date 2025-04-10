49828bdf29ad0e6933c9d63e1e1abd63cf8f1fbf
SHI Shaohua <shishaohua7@163.com>
2025-04-10 13:50:48 +0800

    Update slayer_trimmedversion.py

--- slayer_trimmedversion.py
+++ slayer_trimmedversion.py
@@ -22,7 +22,7 @@ b.get("http://talent.woordee.com/front/truser") #WE登录页
 def login():
     print "logging in..."
     b.find_element_by_id("loginPhone").send_keys("18209347100") #输入手机号
-    b.find_element_by_id("loginPassword").send_keys("ssh19198918") #输入密码
+    b.find_element_by_id("loginPassword").send_keys("s.1") #输入密码
     b.find_element_by_xpath("//*[@onclick='login()']").click() #触发登录
     b.get("http://talent.woordee.com/front/task/taskCenter") #进入"订单中心"页面
     print "successfully logged in\nhunting..."
